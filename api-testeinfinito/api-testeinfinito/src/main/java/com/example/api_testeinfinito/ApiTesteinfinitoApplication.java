b5c43c920f8bc40661e07720ea82c37293be567f
karenads <karenrabelomoreira@gmail.com>
2026-02-26 18:50:33 -0300

    Add files via upload

---
+++ api-testeinfinito/api-testeinfinito/src/main/java/com/example/api_testeinfinito/ApiTesteinfinitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_testeinfinito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesteinfinitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTesteinfinitoApplication.class, args);
+	}
+
+}
